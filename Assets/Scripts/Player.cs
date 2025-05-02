dd2029fa30a440790b476b78198ed07b0b22e7bb
Sahus Gupta <sahusgupta@gmail.com>
2025-05-01 19:24:51 -0500

    jumping and double jumping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,36 +1,72 @@
-using System.Runtime.CompilerServices;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using TMPro;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpHeight = 10f;
+    [SerializeField] private float doubleJumpHeight = 8f;
 
     private Rigidbody rb;
-    private Vector3 movement;
-    [SerializeField] float speed;
-    // Start is called before the first frame update
+    private bool canJump = true;
+    private bool canDoubleJump = false;
+    private bool isGrounded;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
-
     }
-    private void FixedUpdate()
+
+    void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal"); 
-        float verticalInput = Input.GetAxis("Vertical");  
+        CheckGrounded();
+        HandleJumpInput();
+    }
 
-        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
+    void FixedUpdate()
+    {
+        HandleMovement();
+    }
 
+    void CheckGrounded()
+    {
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, 1.1f);
+
+        if (isGrounded)
+        {
+            canJump = true;
+        }
+    }
+
+    void HandleJumpInput()
+    {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(Vector3.up * 10, ForceMode.Force);
+            if (isGrounded)
+            {
+                Jump(jumpHeight);
+                canJump = false;
+                canDoubleJump = true;
+            }
+            else if (canDoubleJump)
+            {
+                Jump(doubleJumpHeight);
+                canDoubleJump = false;
+            }
         }
+    }
 
+    void Jump(float height)
+    {
+        rb.AddForce(Vector3.up * height, ForceMode.Impulse);
+    }
+
+    void HandleMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
         rb.AddForce(movement * speed, ForceMode.VelocityChange);
     }
 }
